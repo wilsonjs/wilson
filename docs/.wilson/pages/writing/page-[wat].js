4f1378b4e2053c2ec46267eeefc642e67f49d503
Christoph Werner <christoph+github@codepunkt.de>
2022-07-19 16:01:54 +0200

    fix: git ignore .wilson directory

--- docs/.wilson/pages/writing/page-[wat].js
+++
@@ -1,7 +0,0 @@
-import { h, Fragment } from "preact";
-export function getRenderedPaths() {
-  return [{ params: { wat: "oink" } }];
-}
-export default function Page() {
-  return /* @__PURE__ */ h("h1", null, "page-[wat]");
-}
