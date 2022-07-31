68d8c20bfe5cadd6f35f957025d41167a2c9b33f
Christoph Werner <christoph+github@codepunkt.de>
2022-07-31 10:14:53 +0200

    refactor: get rid of VNodeSource interface

--- packages/wilson/src/client/app.server.tsx
+++ packages/wilson/src/client/app.server.tsx
@@ -27,23 +27,19 @@ export type IslandsByPath = Record<string, IslandDefinition[]>
 
 let islands: IslandDefinition[] = []
 
-interface VNodeSource {
-  islandPath?: string
-}
-
-function createLazyHydrationWrapper({ islandPath }: VNodeSource) {
-  /**
-   * Prepends a `text/hydration` script tag.
-   */
+/**
+ * Wraps island in a `wilson-island` web component and prepends a
+ * `text/hydration` script tag with a placeholder for hydration code.
+ *
+ * Also constructs an `island` object that is used to replace the
+ * placeholder script tag with the real code performing the partial,
+ * possibly lazy hydration.
+ */
+function createLazyHydrationWrapper(islandPath: string) {
   return function LazyHydrationWrapper({
     children,
     ...props
   }: RenderableProps<{}>) {
-    // didn't find path where the island was imported from as a default import
-    if (islandPath === undefined) {
-      return null
-    }
-
     const no = islands.length + 1
     const island = {
       componentPath: islandPath,
@@ -52,7 +48,7 @@ function createLazyHydrationWrapper({ islandPath }: VNodeSource) {
       script: /* js */ `
         import { hydrateNow } from '@wilson/hydration';
         import { default as component } from '${islandPath}';
-        hydrateNow(component, 'island-${no}', ${JSON.stringify(props)}, {});
+        hydrateNow(component, 'island-${no}', ${JSON.stringify(props)});
       `,
     }
     islands.push(island)
@@ -66,35 +62,32 @@ function createLazyHydrationWrapper({ islandPath }: VNodeSource) {
   }
 }
 
-interface PotentiallyLazyHydratedVNode<
+interface IslandVNode<
   T extends LazyHydrationAttributes = LazyHydrationAttributes,
 > extends VNode<T> {
   wrapped?: true
-  __source?: VNodeSource
+  __source?: { islandPath?: string }
 }
 
 let busy = false
 let counter = 0
 
-async function prepareLazyHydrationHook(vnode: PotentiallyLazyHydratedVNode) {
+async function prepareLazyHydrationHook(vnode: IslandVNode) {
   if (
     vnode.props.clientLoad &&
     !busy &&
     !vnode.wrapped &&
     typeof vnode.type !== 'string' &&
     // components that have clientLoad, but don't have an islandPath in __source
-    {}.toString.call(vnode.__source) === '[object Object]'
+    !!vnode.__source &&
+    vnode.__source.islandPath
   ) {
     // important because cloneElement calls options.vnode
     busy = true
-    // @ts-ignore
-    const clone = cloneElement(
-      vnode,
-      vnode.props,
-    ) as PotentiallyLazyHydratedVNode
+    const clone = cloneElement(vnode, vnode.props) as IslandVNode
     busy = false
     clone.wrapped = true
-    vnode.type = createLazyHydrationWrapper(vnode.__source!)
+    vnode.type = createLazyHydrationWrapper(vnode.__source!.islandPath)
     vnode.props.children = [clone]
     counter = counter + 1
   }
