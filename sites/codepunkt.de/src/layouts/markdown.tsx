df3a2e0f2fa8b7e3a2fd2225584271224c45b950
Christoph Werner <christoph@codepunkt.de>
2023-01-15 22:06:46 +0100

    feat: show post date and taxonomy topics on codepunkt.de site

--- sites/codepunkt.de/src/layouts/markdown.tsx
+++ sites/codepunkt.de/src/layouts/markdown.tsx
@@ -1,14 +1,35 @@
 import type { StaticPageProps } from 'wilson'
+import { frontmatter } from '../pages/about.de'
 import DefaultLayout from './default'
+import styles from './markdown.module.scss'
+
+const formatPostDate = (date: string, lang = 'en'): string | number =>
+  typeof Date.prototype.toLocaleDateString === 'function'
+    ? new Date(date).toLocaleDateString(lang, {
+        day: 'numeric',
+        month: 'long',
+        year: 'numeric',
+      })
+    : date
 
 export default function MarkdownLayout(props: StaticPageProps) {
   return (
     <>
       <DefaultLayout {...props}>
+        <time dateTime={`${props.frontmatter.date}`} class={styles.postdate}>
+          {formatPostDate(props.frontmatter.date, props.language)}
+        </time>
         <h1>{props.frontmatter.title}</h1>
-        {props.frontmatter.description && (
-          <p>{props.frontmatter.description}</p>
+        {props.frontmatter.taxonomies.topics.length && (
+          <ol class={styles.topics}>
+            {(props.frontmatter.taxonomies.topics as string[]).map((topic) => (
+              <li>{topic}</li>
+            ))}
+          </ol>
         )}
+        {/* {props.frontmatter.description && (
+          <p>{props.frontmatter.description}</p>
+        )} */}
         <div id="__wilson-markdown">{props.children}</div>
       </DefaultLayout>
     </>
